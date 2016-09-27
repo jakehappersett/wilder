cf0708fff80e8ffec8b68dad22e0c6cec7ab883c
jakehappersett <jake.happersett@gmail.com>
2016-09-26 21:52:58 -0400

    now deletes old jpg files

--- wilder.py
+++ wilder.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python
+
 import praw
 from os import *
 import urllib
 from subprocess import call
 r = praw.Reddit('gonewild top post bot')
 
+files = os.listdir(os.curdir)
+for f in files: 
+	if f.endswith(jpg) or f.endswith(png):
+		os.remove(f)
+
 while True:
 	sr = r.get_subreddit('gonewild')
-	for submission in sr.get_top(limit=5):
+	for submission in sr.get_top(limit=20):
 		if submission.url[-4:] == '.jpg' or submission.url[-4:] == '.png' or submission.url[-4:] == '.gif':
 			urllib.urlretrieve(submission.url, path.basename(submission.url))#path.basename('/gw'))
 			#call(["feh --bg-scale %s" % path.basename(submission.url)])
